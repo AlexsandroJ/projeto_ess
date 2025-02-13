aaba28230dc1174e809e26aa76dca679d5bdc919
Alexsandro J. da Silva <39406602+AlexsandroJ@users.noreply.github.com>
2025-02-13 11:13:44 -0300

    Update conteudo.js

--- features/step_definitions/conteudo.js
+++ features/step_definitions/conteudo.js
@@ -1,106 +1,100 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
 let movies = [];
+
+function getAllMovies(dataTable) {
+    movies = dataTable.hashes();
+    console.log("\nLista de filmes|séries:\n");
+    movies.forEach(element => {
+        console.log("title: ", element.title, " ratingAverage: ", element.ratingAverage, " releaseYear: ", element.releaseYear, " platform: ", element.platform, " cover: ", element.cover);
+    });
+}
+
+function getAllReviewsForMovie(dataTable) {
+    movies = dataTable.hashes();
+    console.log("\nExemplo de exibição de dados:\n");
+    movies.forEach(element => {
+        console.log("title: ", element.title, " rating: ", element.rating, " reviewText: ", element.reviewText);
+    });
+}
+
+function getReviewsByUser(string,dataTable) {
+    movies = dataTable.hashes();
+    console.log(`\nReviews do usuario ${string}:\n`);
+    movies.forEach(element => {
+        console.log("title: ", element.title, " rating: ", element.rating, " reviewText: ", element.reviewText);
+    });
+}
+
 Given('Exibir onde os filmes|séries estão disponíveis', function (dataTable) {
     // Write code here that turns the phrase above into concrete actions
-    movies = dataTable.hashes();
-    console.log("disponibilidade\n");
-    movies.forEach(element => {
-        console.log("Título: ",element.Título," Nota: ",element.Nota," Ano: ",element.Ano," Plataforma: ",element.Plataforma," Capa: ",element.Capa );
-      });
+    getAllMovies(dataTable);
 });
-
 
 When('exibe a disponibilidade dos filmes|séries', function () {
     // Write code here that turns the phrase above into concrete actions
-    console.log('exibe a disponibilidade dos filmes|séries\n');
+    console.log('\nExibe a disponibilidade dos filmes|séries\n');
 });
 
 
 Then('devo ver a seguinte disponibilidade:', function (dataTable) {
     // Write code here that turns the phrase above into concrete actions
-    movies = dataTable.hashes();
-    console.log("disponibilidade\n");
-    movies.forEach(element => {
-        console.log("Título: ",element.Título," Nota: ",element.Nota," Ano: ",element.Ano," Plataforma: ",element.Plataforma," Capa: ",element.Capa );
-      });
+    getAllMovies(dataTable);
 });
-
 
 Given('os seguintes filmes|séries existem:', function (dataTable) {
     // Write code here that turns the phrase above into concrete actions
-    movies = dataTable.hashes();
-    console.log("os seguintes filmes|séries existem:\n");
-    movies.forEach(element => {
-        console.log("Título: ",element.Título," Ano: ",element.Ano," Plataforma: ",element.Plataforma );
-      });
+    getAllMovies(dataTable);
 });
 
-When('adiciono um novo filme|série com título {string}, ano {string}, nota {string}, capa {string} e plataforma {string}', function (string, string2, string3, string4, string5) {
+When('adiciono um novo filme|série com title {string}, releaseYear {string}, ratingAverage {string}, cover {string} e platform {string}', function (string, string2, string3, string4, string5) {
     // Write code here that turns the phrase above into concrete actions
-    console.log(` Titulo: ${string} Ano: ${string2} Nota: ${string3} Capa: ${string4} Plataforma: ${string5}`);
+    console.log(`\nAdiciono um novo filme|série com title ${string}, releaseYear ${string2}, ratingAverage ${string3}, cover ${string4} e platform ${string5}`);
 });
 
 Then('os seguintes filmes|séries devem existir:', function (dataTable) {
     // Write code here that turns the phrase above into concrete actions
-    movies = dataTable.hashes();
-    console.log("os seguintes filmes|séries devem existir:\n");
-    movies.forEach(element => {
-        console.log("Título: ",element.Título," Nota: ",element.Nota," Ano: ",element.Ano," Plataforma: ",element.Plataforma," Capa: ",element.Capa );
-      });
+    getAllMovies(dataTable);
+});
+
+When('edito o filme|série com title {string} para ter a platform {string}', function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    console.log(`\nEdito o filme|série com title: ${string} para ter a platform: ${string2}`);
 });
 
 
-
-When('edito o filme|série com título {string} para ter a plataforma {string}', function (string, string2) {
+When('remover o filme|série com title {string}', function (string) {
     // Write code here that turns the phrase above into concrete actions
-    console.log('...');
+    console.log(`\nRemover o filme|série com title ${string}`);
 });
 
-
-When('remover o filme|série com título {string}', function (string) {
+Given('os seguintes reviews criadas pelo usuário {string} existem:', function (string, dataTable) {
     // Write code here that turns the phrase above into concrete actions
-    console.log('...');
+    getReviewsByUser(string,dataTable)
 });
 
-
-Given('os seguintes reviews criadas pelo usuário existem:', function (dataTable) {
+When('editar o review de {string} para ter rating {string} e reviewText {string}', function (string, string2, string3) {
     // Write code here that turns the phrase above into concrete actions
-    console.log('...');
-});
-
-When('editar o review de {string} para ter avaliação {string} e comentário {string}', function (string, string2, string3) {
-    // Write code here that turns the phrase above into concrete actions
-    console.log('...');
+    console.log(`\nEditar o review de ${string} para ter rating ${string2} e reviewText ${string3}`);
 });
 
 Then('os seguintes reviews devem existir:', function (dataTable) {
     // Write code here that turns the phrase above into concrete actions
-    movies = dataTable.hashes();
-    console.log("os seguintes reviews devem existir:\n");
-    movies.forEach(element => {
-        console.log("Título: ",element.Título," Avaliação: ",element['Avaliação']," Comentário: ",element['Comentário'] );
-      });
+    getAllReviewsForMovie(dataTable);
 });
 
-
-When('adicionar um novo review para {string} com avaliação {string} e comentário {string}', function (string, string2, string3) {
+When('adicionar um novo review para {string} com rating {string} e reviewText {string}', function (string, string2, string3) {
     // Write code here that turns the phrase above into concrete actions
-    console.log('...');
+    console.log(`Adicionar um novo review para ${string} com rating ${string2} e reviewText ${string3}`);
 });
-
 
 Given('os seguintes reviews existem:', function (dataTable) {
     // Write code here that turns the phrase above into concrete actions
-    movies = dataTable.hashes();
-    console.log("os seguintes reviews existem:\n");
-    movies.forEach(element => {
-        console.log("Título: ",element.Título," Avaliação: ",element['Avaliação']," Comentário: ",element['Comentário'] );
-      });
+    getAllReviewsForMovie(dataTable);
 });
 
 When('remover o review {string}', function (string) {
     // Write code here that turns the phrase above into concrete actions
-    console.log(`remover o review ${string}`);
+    console.log(`\nRemover o review ${string}`);
 });
 
